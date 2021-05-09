85614087827d917b79a700754c621493bec41b1f
NeaEnot <oleg.kozloss@yandex.ru>
2021-05-09 17:41:34 +0400

    Интерфейс IOrderLogic

---
+++ Core/Interfaces/IOrderLogic.cs
@@ -0,0 +1,17 @@
+﻿using Core.Models.Binding;
+using Core.Models.View;
+using System.Collections.Generic;
+
+namespace Core.Interfaces
+{
+    public interface IOrderLogic
+    {
+        void Create(OrderBinding model);
+
+        List<OrderView> Read(OrderBinding model);
+
+        void Update(OrderBinding model);
+
+        void Delete(OrderBinding model);
+    }
+}
