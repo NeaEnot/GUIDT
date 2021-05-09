d98869062f28819af772a098506130acc9d1c4af
NeaEnot <oleg.kozloss@yandex.ru>
2021-05-09 17:44:12 +0400

    Интерфейс IProductLogic

---
+++ Core/Interfaces/IProductLogic.cs
@@ -0,0 +1,17 @@
+﻿using Core.Models.Binding;
+using Core.Models.View;
+using System.Collections.Generic;
+
+namespace Core.Interfaces
+{
+    public interface IProductLogic
+    {
+        void Create(ProductBinding model);
+
+        List<ProductView> Read(ProductBinding model);
+
+        void Update(ProductBinding model);
+
+        void Delete(ProductBinding model);
+    }
+}
