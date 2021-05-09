e856f0dcd9745fb9e91f71260eff195ae9daf9ac
NeaEnot <oleg.kozloss@yandex.ru>
2021-05-09 15:45:35 +0400

    Добавлено поле для OrderProductView в модель OrderView

--- Core/Models/View/OrderView.cs
+++ Core/Models/View/OrderView.cs
@@ -1,9 +1,13 @@
-﻿namespace Core.Models.View
+﻿using System.Collections.Generic;
+
+namespace Core.Models.View
 {
     public class OrderView
     {
         public int Id { get; set; }
 
         public int Sum { get; set; }
+
+        public List<OrderProductView> OrderProducts { get; set; }
     }
 }
--- Test/CoreTest/ViewModelsTest.cs
+++ Test/CoreTest/ViewModelsTest.cs
@@ -1,4 +1,5 @@
 ﻿using Core.Models.View;
+using System.Collections.Generic;
 using Xunit;
 
 namespace Test.CoreTest
@@ -25,7 +26,7 @@ namespace Test.CoreTest
         }
 
         [Fact]
-        public void OrderProductView()
+        public void TestOrderProductView()
         {
             OrderProductView opv = new OrderProductView { Id = 0, OrderId = 0, ProductId = 0, Count = 10 };
 
@@ -33,6 +34,19 @@ namespace Test.CoreTest
             Assert.Equal(0, opv.OrderId);
             Assert.Equal(0, opv.ProductId);
             Assert.Equal(10, opv.Count);
+        }
+
+        [Fact]
+        public void TestOrderProductsInOrderView()
+        {
+            OrderView ov = new OrderView { Id = 0, Sum = 1000, OrderProducts = new List<OrderProductView>() };
+            ov.OrderProducts.Add(new OrderProductView { Id = 0, OrderId = 0, ProductId = 0, Count = 10 });
+            ov.OrderProducts.Add(new OrderProductView { Id = 1, OrderId = 0, ProductId = 1, Count = 5 });
+
+            Assert.Equal(0, ov.OrderProducts[0].ProductId);
+            Assert.Equal(1, ov.OrderProducts[1].ProductId);
+            Assert.Equal(10, ov.OrderProducts[0].Count);
+            Assert.Equal(5, ov.OrderProducts[1].Count);
         }
     }
 }
