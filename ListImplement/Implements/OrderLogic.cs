25fca353207c3bd502067fcf8336f28d393d2493
NeaEnot <oleg.kozloss@yandex.ru>
2021-05-10 16:45:56 +0400

    Метод ListImplement.Implements.OrderLogic.Read

---
+++ ListImplement/Implements/OrderLogic.cs
@@ -0,0 +1,14 @@
+﻿using Core.Models.Binding;
+using Core.Models.View;
+using System.Collections.Generic;
+
+namespace ListImplement.Implements
+{
+    public class OrderLogic
+    {
+        public List<OrderView> Read(OrderBinding model)
+        {
+            return new List<OrderView>();
+        }
+    }
+}
---
+++ Test/ListImplementTest/OrderLogicTest.cs
@@ -0,0 +1,20 @@
+﻿using Core.Models.View;
+using ListImplement.Implements;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Test.ListImplementTest
+{
+    public class OrderLogicTest
+    {
+        [Fact]
+        public void TestRead()
+        {
+            OrderLogic logic = new OrderLogic();
+
+            List<OrderView> list = logic.Read(null);
+
+            Assert.Empty(list);
+        }
+    }
+}
