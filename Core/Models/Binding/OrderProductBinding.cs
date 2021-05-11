46b08f1c31eb2694ec3825c5a4e009113fca1c17
NeaEnot <oleg.kozloss@yandex.u>
2021-05-11 13:46:54 +0300

    Удалить OrderProductBinding.Id

--- Core/Models/Binding/OrderProductBinding.cs
+++ Core/Models/Binding/OrderProductBinding.cs
@@ -2,8 +2,6 @@
 {
     public class OrderProductBinding
     {
-        public int Id { get; set; }
-
         public int OrderId { get; set; }
 
         public int ProductId { get; set; }
--- Test/CoreTest/BindingModelsTest.cs
+++ Test/CoreTest/BindingModelsTest.cs
@@ -9,37 +9,36 @@ namespace Test.CoreTest
         [Fact]
         public void TestProductBinding()
         {
-            ProductBinding pv = new ProductBinding { Id = 0, Name = "Test", Price = 100 };
+            ProductBinding pb = new ProductBinding { Id = 0, Name = "Test", Price = 100 };
 
-            Assert.Equal(0, pv.Id);
-            Assert.Equal("Test", pv.Name);
-            Assert.Equal(100, pv.Price);
+            Assert.Equal(0, pb.Id);
+            Assert.Equal("Test", pb.Name);
+            Assert.Equal(100, pb.Price);
         }
 
         [Fact]
         public void TestOrderProductBinding()
         {
-            OrderProductBinding opv = new OrderProductBinding { Id = 0, OrderId = 0, ProductId = 0, Count = 10, Price = 100 };
+            OrderProductBinding opb = new OrderProductBinding { OrderId = 0, ProductId = 0, Count = 10, Price = 100 };
 
-            Assert.Equal(0, opv.Id);
-            Assert.Equal(0, opv.OrderId);
-            Assert.Equal(0, opv.ProductId);
-            Assert.Equal(10, opv.Count);
-            Assert.Equal(100, opv.Price);
+            Assert.Equal(0, opb.OrderId);
+            Assert.Equal(0, opb.ProductId);
+            Assert.Equal(10, opb.Count);
+            Assert.Equal(100, opb.Price);
         }
 
         [Fact]
         public void TestOrderBinding()
         {
-            OrderBinding ov = new OrderBinding { Id = 0, OrderProducts = new List<OrderProductBinding>() };
-            ov.OrderProducts.Add(new OrderProductBinding { Id = 0, OrderId = 0, ProductId = 0, Count = 10 });
-            ov.OrderProducts.Add(new OrderProductBinding { Id = 1, OrderId = 0, ProductId = 1, Count = 5 });
+            OrderBinding ob = new OrderBinding { Id = 0, OrderProducts = new List<OrderProductBinding>() };
+            ob.OrderProducts.Add(new OrderProductBinding { OrderId = 0, ProductId = 0, Count = 10 });
+            ob.OrderProducts.Add(new OrderProductBinding { OrderId = 0, ProductId = 1, Count = 5 });
 
-            Assert.Equal(0, ov.Id);
-            Assert.Equal(0, ov.OrderProducts[0].ProductId);
-            Assert.Equal(1, ov.OrderProducts[1].ProductId);
-            Assert.Equal(10, ov.OrderProducts[0].Count);
-            Assert.Equal(5, ov.OrderProducts[1].Count);
+            Assert.Equal(0, ob.Id);
+            Assert.Equal(0, ob.OrderProducts[0].ProductId);
+            Assert.Equal(1, ob.OrderProducts[1].ProductId);
+            Assert.Equal(10, ob.OrderProducts[0].Count);
+            Assert.Equal(5, ob.OrderProducts[1].Count);
         }
     }
 }
