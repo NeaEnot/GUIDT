8fd686f0b27560630e2b277647485856b0408b83
NeaEnot <oleg.kozloss@yandex.ru>
2021-05-12 03:06:12 +0400

    Создать атрибут для View моделей

---
+++ Core/Attributes/ColumnAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Core.Attributes
+{
+    public class ColumnAttribute : Attribute
+    {
+        public ColumnAttribute(string title = "", bool visible = true)
+        {
+            Title = title;
+            Visible = visible;
+        }
+
+        public string Title { get; private set; }
+
+        public bool Visible { get; private set; }
+    }
+}
---
+++ Test/CoreTest/AttributesTest.cs
@@ -0,0 +1,17 @@
+﻿using Core.Attributes;
+using Xunit;
+
+namespace Test.CoreTest
+{
+    public class AttributesTest
+    {
+        [Fact]
+        public void TestColumnAttribute()
+        {
+            ColumnAttribute attribute = new ColumnAttribute("Test", true);
+
+            Assert.Equal("Test", attribute.Title);
+            Assert.True(attribute.Visible);
+        }
+    }
+}
