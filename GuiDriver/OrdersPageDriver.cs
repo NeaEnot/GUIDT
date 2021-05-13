83e33907f04f3041596482724c450cf53b0443ab
NeaEnot <oleg.kozloss@yandex.ru>
2021-05-13 23:18:17 +0400

    Отображение заказов

---
+++ GuiDriver/OrdersPageDriver.cs
@@ -0,0 +1,13 @@
+﻿using Core.Models.View;
+using System.Collections.Generic;
+
+namespace GuiDriver
+{
+    public class OrdersPageDriver
+    {
+        public List<OrderView> GetAllOrders()
+        {
+            return new List<OrderView>();
+        }
+    }
+}
---
+++ Test/GuiDriverTest/OrdersPageDriverTest.cs
@@ -0,0 +1,20 @@
+﻿using Core.Models.View;
+using GuiDriver;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Test.GuiDriverTest
+{
+    public class OrdersPageDriverTest
+    {
+        [Fact]
+        public void TestOrdersListEmpty()
+        {
+            OrdersPageDriver driver = new OrdersPageDriver();
+
+            List<OrderView> list = driver.GetAllOrders();
+
+            Assert.Empty(list);
+        }
+    }
+}
