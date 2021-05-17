a8ee682ea71b6efeba3d4f9ca8bfa73dfa8facd8
NeaEnot <oleg.kozloss@yandex.ru>
2021-05-17 04:50:35 +0400

    Класс для создания заказа Поле для Id

---
+++ Test/UiDriverTest/OrderPageDriverTest.cs
@@ -0,0 +1,19 @@
+﻿using ListImplement.Implements;
+using UiDriver;
+using Xunit;
+
+namespace Test.UiDriverTest
+{
+    public class OrderPageDriverTest
+    {
+        [Fact]
+        public void TestConstructor()
+        {
+            OrderPageDriver driver1 = new OrderPageDriver(new UiContext(new OrderLogic(), new ProductLogic()), 10);
+            OrderPageDriver driver2 = new OrderPageDriver(new UiContext(new OrderLogic(), new ProductLogic()), null);
+
+            Assert.Equal(10, driver1.orderId);
+            Assert.Equal(null, driver2.orderId);
+        }
+    }
+}
---
+++ UiDriver/OrderPageDriver.cs
@@ -0,0 +1,12 @@
+﻿namespace UiDriver
+{
+    public class OrderPageDriver
+    {
+        public int? orderId;
+
+        public OrderPageDriver(UiContext context, int? orderId)
+        {
+            this.orderId = orderId;
+        }
+    }
+}
