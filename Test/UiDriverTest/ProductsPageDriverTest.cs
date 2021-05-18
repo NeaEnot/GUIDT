ee44bed617803fabd1c3d716370cc5574341246c
NeaEnot <oleg.kozloss@yandex.u>
2021-05-18 15:09:08 +0300

    Получение всех продуктов

--- Test/UiDriverTest/ProductsPageDriverTest.cs
+++ Test/UiDriverTest/ProductsPageDriverTest.cs
@@ -1,4 +1,5 @@
-﻿using Core.Models.View;
+﻿using Core.Models.Binding;
+using Core.Models.View;
 using ListImplement.Implements;
 using System.Collections.Generic;
 using UiDriver;
@@ -16,6 +17,32 @@ namespace Test.UiDriverTest
             List<ProductView> list = driver.GetAllProducts();
 
             Assert.Empty(list);
+        }
+
+        [Fact]
+        public void TestGetAllProducts()
+        {
+            ProductLogic logic = new ProductLogic();
+
+            try
+            {
+                logic.Create(new ProductBinding { Name = "Test1", Price = 10 });
+                logic.Create(new ProductBinding { Name = "Test2", Price = 15 });
+
+                OrderProductPageDriver driver = new OrderProductPageDriver(new UiContext(new OrderLogic(), logic), new OrderView(), null);
+
+                List<ProductView> list = driver.GetAllProducts();
+
+                Assert.Equal(2, list.Count);
+                Assert.Equal("Test1", list[0].Name);
+                Assert.Equal(10, list[0].Price);
+                Assert.Equal("Test2", list[1].Name);
+                Assert.Equal(15, list[1].Price);
+            }
+            finally
+            {
+                logic.Delete(null);
+            }
         }
     }
 }
