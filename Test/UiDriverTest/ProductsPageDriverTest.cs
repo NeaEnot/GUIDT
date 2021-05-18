77456f277fd30fdf6b9a1be7de8e1c41b7424dd7
NeaEnot <oleg.kozloss@yandex.u>
2021-05-18 15:07:58 +0300

    Получение пустого списка продуктов

---
+++ Test/UiDriverTest/ProductsPageDriverTest.cs
@@ -0,0 +1,21 @@
+﻿using Core.Models.View;
+using ListImplement.Implements;
+using System.Collections.Generic;
+using UiDriver;
+using Xunit;
+
+namespace Test.UiDriverTest
+{
+    public class ProductsPageDriverTest
+    {
+        [Fact]
+        public void TestGetAllProductsEmpty()
+        {
+            ProductsPageDriver driver = new ProductsPageDriver(new UiContext(new OrderLogic(), new ProductLogic()));
+
+            List<ProductView> list = driver.GetAllProducts();
+
+            Assert.Empty(list);
+        }
+    }
+}
---
+++ UiDriver/ProductsPageDriver.cs
@@ -0,0 +1,16 @@
+﻿using Core.Models.View;
+using System.Collections.Generic;
+
+namespace UiDriver
+{
+    public class ProductsPageDriver : PageDriver
+    {
+        public ProductsPageDriver(UiContext context) : base(context)
+        { }
+
+        public List<ProductView> GetAllProducts()
+        {
+            return context.ProductLogic.Read(null);
+        }
+    }
+}
