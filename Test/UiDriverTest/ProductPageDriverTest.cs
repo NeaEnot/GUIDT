f07911cc049369c30f046a511467bb99444ce946
NeaEnot <oleg.kozloss@yandex.ru>
2021-05-19 21:53:08 +0400

    Класс для создания продукта Поле для названия

---
+++ Test/UiDriverTest/ProductPageDriverTest.cs
@@ -0,0 +1,20 @@
+﻿using ListImplement.Implements;
+using UiDriver;
+using Xunit;
+
+namespace Test.UiDriverTest
+{
+    public class ProductPageDriverTest
+    {
+        [Fact]
+        public void TestProductName()
+        {
+            ProductPageDriver driver = new ProductPageDriver(new UiContext(new OrderLogic(), new ProductLogic()), null);
+            driver.ProductName = () => "Ananas";
+
+            string msg = driver.ProductName();
+
+            Assert.Equal("Ananas", msg);
+        }
+    }
+}
---
+++ UiDriver/ProductPageDriver.cs
@@ -0,0 +1,19 @@
+﻿using Core.Models.View;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UiDriver
+{
+    public class ProductPageDriver : PageDriver
+    {
+        #region сustomizableMethods
+        public Func<string> ProductName { get; set; }
+        #endregion
+
+        public ProductPageDriver(UiContext context, ProductView product) : base(context)
+        {
+
+        }
+    }
+}
