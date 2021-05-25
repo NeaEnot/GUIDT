154c5e869805583e1f29e28ba98df267428d3e7a
NeaEnot <oleg.kozloss@yandex.ru>
2021-05-25 23:36:06 +0400

    WinForms.FormOrder

--- WinForms/FormOrder.Designer.cs
+++ WinForms/FormOrder.Designer.cs
@@ -30,6 +30,10 @@ namespace WinForms
         private void InitializeComponent()
         {
             this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonDeleteOrderProduct = new System.Windows.Forms.Button();
+            this.buttonUpdateOrderProduct = new System.Windows.Forms.Button();
+            this.buttonCreateOrderProduct = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
             this.SuspendLayout();
             // 
@@ -42,14 +46,59 @@ namespace WinForms
             this.dataGridView.Size = new System.Drawing.Size(571, 372);
             this.dataGridView.TabIndex = 0;
             // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(12, 395);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(75, 23);
+            this.buttonSave.TabIndex = 1;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // buttonDeleteOrderProduct
+            // 
+            this.buttonDeleteOrderProduct.Location = new System.Drawing.Point(488, 394);
+            this.buttonDeleteOrderProduct.Name = "buttonDeleteOrderProduct";
+            this.buttonDeleteOrderProduct.Size = new System.Drawing.Size(95, 23);
+            this.buttonDeleteOrderProduct.TabIndex = 2;
+            this.buttonDeleteOrderProduct.Text = "Delete product";
+            this.buttonDeleteOrderProduct.UseVisualStyleBackColor = true;
+            this.buttonDeleteOrderProduct.Click += new System.EventHandler(this.buttonDeleteOrderProduct_Click);
+            // 
+            // buttonUpdateOrderProduct
+            // 
+            this.buttonUpdateOrderProduct.Location = new System.Drawing.Point(381, 394);
+            this.buttonUpdateOrderProduct.Name = "buttonUpdateOrderProduct";
+            this.buttonUpdateOrderProduct.Size = new System.Drawing.Size(101, 23);
+            this.buttonUpdateOrderProduct.TabIndex = 3;
+            this.buttonUpdateOrderProduct.Text = "Update product";
+            this.buttonUpdateOrderProduct.UseVisualStyleBackColor = true;
+            this.buttonUpdateOrderProduct.Click += new System.EventHandler(this.buttonUpdateOrderProduct_Click);
+            // 
+            // buttonCreateOrderProduct
+            // 
+            this.buttonCreateOrderProduct.Location = new System.Drawing.Point(276, 394);
+            this.buttonCreateOrderProduct.Name = "buttonCreateOrderProduct";
+            this.buttonCreateOrderProduct.Size = new System.Drawing.Size(99, 23);
+            this.buttonCreateOrderProduct.TabIndex = 4;
+            this.buttonCreateOrderProduct.Text = "Create product";
+            this.buttonCreateOrderProduct.UseVisualStyleBackColor = true;
+            this.buttonCreateOrderProduct.Click += new System.EventHandler(this.buttonCreateOrderProduct_Click);
+            // 
             // FormOrder
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(596, 450);
+            this.ClientSize = new System.Drawing.Size(596, 430);
+            this.Controls.Add(this.buttonCreateOrderProduct);
+            this.Controls.Add(this.buttonUpdateOrderProduct);
+            this.Controls.Add(this.buttonDeleteOrderProduct);
+            this.Controls.Add(this.buttonSave);
             this.Controls.Add(this.dataGridView);
             this.Name = "FormOrder";
             this.Text = "FormOrder";
+            this.Load += new System.EventHandler(this.FormOrder_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
             this.ResumeLayout(false);
 
@@ -58,5 +107,9 @@ namespace WinForms
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button buttonDeleteOrderProduct;
+        private System.Windows.Forms.Button buttonUpdateOrderProduct;
+        private System.Windows.Forms.Button buttonCreateOrderProduct;
     }
 }
--- WinForms/FormOrder.cs
+++ WinForms/FormOrder.cs
@@ -1,9 +1,12 @@
-﻿using Core.Models.View;
+﻿using Core.Attributes;
+using Core.Models.View;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Windows.Forms;
 using UiDriver;
@@ -34,6 +37,54 @@ namespace WinForms
             driver.ShowErrorMessage = (msg) => { MessageBox.Show(msg, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error); };
 
             driver.MoveToOrderProductPage = null;
+        }
+
+        private void FormOrder_Load(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            List<OrderProductView> list = driver.GetAllOrderProducts();
+            dataGridView.DataSource = list;
+            ConfigureDataGrid();
+        }
+
+        private void ConfigureDataGrid()
+        {
+            dataGridView.Columns.Clear();
+
+            Type type = typeof(OrderProductView);
+
+            foreach (PropertyInfo prop in type.GetProperties().Where(rec => rec.GetCustomAttributes<ColumnAttribute>().Count() > 0))
+            {
+                dataGridView.Columns.Add(prop.Name, prop.GetCustomAttribute<ColumnAttribute>().Title);
+                dataGridView.Columns[dataGridView.Columns.Count - 1].Visible = prop.GetCustomAttribute<ColumnAttribute>().Visible;
+            }
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            if (driver.SaveOrder())
+            {
+                Close();
+            }
+        }
+
+        private void buttonCreateOrderProduct_Click(object sender, EventArgs e)
+        {
+            driver.AddOrderProduct();
+        }
+
+        private void buttonUpdateOrderProduct_Click(object sender, EventArgs e)
+        {
+            driver.UpdateOrderProduct();
+        }
+
+        private void buttonDeleteOrderProduct_Click(object sender, EventArgs e)
+        {
+            driver.DeleteOrderProduct();
         }
     }
 }
